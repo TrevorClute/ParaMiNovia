e05b72ceb8ef737a9922ec70f5f1f5d9edc0718a
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2024-05-30 11:20:05 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// import { defineConfig } from "vite";
-// import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [mkcert()],
-//   server: { https: true }, // Not needed for Vite 5+
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [mkcert()],
+  server: { https: true }, // Not needed for Vite 5+
+});
