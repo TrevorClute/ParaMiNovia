7e52c0dad9945ac84d4ff79ff38020b8c256182f
TrevorClute <121002446+TrevorClute@users.noreply.github.com>
2025-03-23 17:45:18 -0400

    dd

--- sorry.js
+++ sorry.js
@@ -27,11 +27,11 @@ async function init() {
     loadingMessage.style.color = "red";
   }
   await Promise.all([
-    faceapi.nets.ssdMobilenetv1.loadFromDisk("models"),
-    faceapi.nets.faceLandmark68Net.loadFromDisk("./models"),
-    faceapi.nets.faceRecognitionNet.loadFromDisk("./models"),
-    faceapi.nets.ageGenderNet.loadFromDisk("./models"),
-    faceapi.nets.faceExpressionNet.loadFromDisk("./models"),
+    faceapi.nets.ssdMobilenetv1.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.ageGenderNet.loadFromUri("./models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("./models"),
   ]);
 
   const canvas = document.getElementById("camera-canvas");
